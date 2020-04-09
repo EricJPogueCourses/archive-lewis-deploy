52344729965de3cc53de21e0227b89ac342dc470
EricJPogue <epogue@epogue.com>
2020-04-09 10:42:06 -0500

    Add initial files.

---
+++ precache-manifest.02cec6d3534f1cd74f07bc8e8eb23246.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5906e32552a3c97e55dfcfecddb916e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d91fb1f8478ab4fe70e9",
+    "url": "/static/css/main.f2ffd57a.chunk.css"
+  },
+  {
+    "revision": "558e7a2754a2a3aa4ff8",
+    "url": "/static/js/2.b8d5ba1d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b8d5ba1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d91fb1f8478ab4fe70e9",
+    "url": "/static/js/main.b3281eeb.chunk.js"
+  },
+  {
+    "revision": "300cc9f92aa702f73955",
+    "url": "/static/js/runtime-main.b36198d6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
